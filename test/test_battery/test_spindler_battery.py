a31d160eff98f9b28bebdf8e33815f1e1a96013d
Suhani Mittal <suhani.mittal@iitgn.ac.in>
2024-05-09 15:23:05 +0530

    unit test cases added

---
+++ test/test_battery/test_spindler_battery.py
@@ -0,0 +1,21 @@
+import unittest
+import datetime
+from battery.spindler_battery import SpindlerBattery
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+
+        battery = SpindlerBattery(today, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+
+        battery = SpindlerBattery(today, last_service_date)
+        self.assertFalse(battery.needs_service())
+    
+if __name__ == '__main__':
+    unittest.main()
